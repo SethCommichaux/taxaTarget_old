2a3f61cd53e6f568d1257603065673c0bd2e4545
Seth.Commichaux <yofi01@gmail.com>
2021-09-07 13:34:41 -0400

    adding createDB scripts to git repo

--- run_pipeline_scripts/run_protist_pipeline_fda.sh
+++ run_pipeline_scripts/run_protist_pipeline_fda.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name=taxaTarget
-#SBATCH -t 7-0:00              # time limit: (D-HH:MM)
-#SBATCH --mem=120G            # memory per node in MB
+#SBATCH -t 1-0:00              # time limit: (D-HH:MM)
+#SBATCH --mem=20G            # memory per node in MB
 #SBATCH --nodes=1              # number of nodes
 #SBATCH --cpus-per-task=12
 
@@ -40,27 +40,20 @@ cd $out
 
 # Run kaiju to query fastq reads against protein sequence binning databse (binningDB.fasta)
 #
-kaijux -f $kaijuDB -i ../$reads_fastq -z 12 -m 9 | grep "^C" > kaiju
+# kaijux -f $kaijuDB -i ../$reads_fastq -z 12 -m 9 | grep "^C" > kaiju
 
 
 # Extract reads that aligned to binning database
 #
-python $run_pipeline/extract_kaiju_reads.py -k kaiju -s ../$reads_fastq -o kaiju.fasta
+# python $run_pipeline/extract_kaiju_reads.py -k kaiju -s ../$reads_fastq -o kaiju.fasta
 
 
 # Align binned reads, with Diamond, to queryDB
 #
 # --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen
-diamond blastx --top 0 --sensitive --min-score 55 --db $queryDB --query kaiju.fasta --threads 12 --outfmt 6 --out kaiju.fasta.diamond
+# diamond blastx --top 0 --sensitive --min-score 55 --db $queryDB --query kaiju.fasta --threads 12 --outfmt 6 --out kaiju.fasta.diamond
 
 
 # Classify reads
 #
-python $run_pipeline/classify_reads_strict.py -d kaiju.fasta.diamond -dir $protist_data/ -p 0.02
-
-
-
-
-
-
-
+python $run_pipeline/classify_reads_strict.py.beta -d kaiju.fasta.diamond -dir $protist_data/ -p 0.3
